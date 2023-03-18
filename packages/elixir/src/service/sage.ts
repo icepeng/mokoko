ab93efa56ef1d0c08febd149697181507514b494
icepeng <typ0@naver.com>
2023-03-18 12:55:24 +0900

    fix: 리롤시 나왔던거 안나오게

--- packages/elixir/src/service/sage.ts
+++ packages/elixir/src/service/sage.ts
@@ -1,3 +1,4 @@
+import { query } from "../api";
 import { GameState } from "../model/game";
 import { CouncilService } from "./council";
 
@@ -25,8 +26,48 @@ export function createSageService(councilService: CouncilService) {
     };
   }
 
+  function rerollCouncils(state: GameState): GameState {
+    if (
+      state.turnLeft === 1 &&
+      [0, 1, 2].every((i) =>
+        ["seal", "exhausted"].includes(query.game.getCouncilType(state, i))
+      )
+    ) {
+      return { ...state };
+    }
+
+    const council1 = councilService.pick(state, 0, [state.sages[0].councilId]);
+    const council2 = councilService.pick(state, 1, [
+      state.sages[1].councilId,
+      council1,
+    ]);
+    const council3 = councilService.pick(state, 2, [
+      state.sages[2].councilId,
+      council1,
+      council2,
+    ]);
+    return {
+      ...state,
+      sages: [
+        {
+          ...state.sages[0],
+          councilId: council1,
+        },
+        {
+          ...state.sages[1],
+          councilId: council2,
+        },
+        {
+          ...state.sages[2],
+          councilId: council3,
+        },
+      ],
+    };
+  }
+
   return {
     updateCouncils,
+    rerollCouncils,
   };
 }
 
